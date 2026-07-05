a798d9e5af3af3e2246281c65ee0351223d85f20
HughWilliams <thuantruongtran0905@gmail.com>
2026-07-05 20:39:40 +0700

    Fix handleXError function to suppress unused parameter warning

--- experiences/getWindows.cpp
+++ experiences/getWindows.cpp
@@ -1,10 +1,12 @@
+// experiences/getWindows.cpp
+
 #include <iostream>
 #include <X11/Xlib.h>
 #include "header/getWindows.h"
-
 using namespace std;
 
-int handleXError(Display* dpy, XErrorEvent* error) {
+// Fix: Suppress warning bằng cách đánh dấu parameter không dùng
+int handleXError([[maybe_unused]] Display* dpy, XErrorEvent* error) {
     if (error->error_code == BadWindow) return 0;
     return 0;
 }
@@ -17,7 +19,7 @@ void getWindows() {
     }
 
     XSetErrorHandler(handleXError);
-
+    
     Window root = DefaultRootWindow(dpy);
     Window parent;
     Window *children = NULL;
@@ -30,13 +32,12 @@ void getWindows() {
     }
 
     cout << "Number of windows: " << nchildren << endl;
+
     for (unsigned int i = 0; i < nchildren; ++i) {
         XWindowAttributes attr;
-
         if (XGetWindowAttributes(dpy, children[i], &attr)) {
             if (attr.map_state == IsViewable) {
                 char* window_name = NULL;
-
                 XFetchName(dpy, children[i], &window_name);
                 if (window_name) {
                     cout << "Window ID: " << children[i] << ", Name: " << window_name << endl;
